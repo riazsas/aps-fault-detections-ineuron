65661253c54613976a6337eab77aa7aa17ee695f
riazas <riazsadique96@gmail.com>
2022-11-26 17:31:07 +0530

    removed a blank space

--- data_dump.py
+++ data_dump.py
@@ -7,7 +7,6 @@ DATA_FILE_PATH ="/config/workspace/aps_failure_training_set1.csv"
 DATABASE_NAME="aps"
 COLLECTION_NAME="sensor"
 
-
 if __name__ == "__main__":
     df = pd.read_csv(DATA_FILE_PATH)
     print(f"Rows and Columns: {df.shape}")
